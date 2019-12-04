7f521b3154bcd3e3cad271627481b4139af2aa97
Monica Damasceno <monicadamasceno@gmail.com>
2019-12-04 17:36:52 -0200

    es6 functions added

---
+++ functions.js
@@ -0,0 +1,180 @@
+//*********************************** EXAMPLE ***********************************//
+// adds 2 to inputted number
+// ex: addTwo(5); -> 7
+// ex: addTwo(6); -> 8
+//*** ES5 ***//
+function addTwo(number) {
+    const result = number + 2;
+    return result;
+}
+// OR
+function addTwo(number) {
+    return number + 2;
+    
+}
+//*** ES6 ***//
+const addTwo = number => {
+    const result = number + 2;
+    return result;
+}
+// OR
+const addTwo = number => number + 2;
+
+//*********************************** PRACTICE ***********************************//
+
+// adds the two inputted numbers together
+// ex: add(5, 10); -> 15
+//*** ES5 ***//
+function add(num1, num2) {
+
+}
+//*** ES6 ***//
+const add = (num1,num2) => // write your code here
+
+//--------------------------------------------------------------------------------//
+// adds all three inputted numbers together
+// ex: addd(1,2,3); -> 6
+// ex: addd(2,4,2); -> 8
+//*** ES5 ***//
+function addd(value1, value2, value3) {
+
+}
+//*** ES6 ***//
+const addd = (value1, value2, value3) => // write your code here
+
+//--------------------------------------------------------------------------------//
+// subtracts the second inputted number from the first inputted number
+// ex: subtract(5, 10); -> -5
+//*** ES5 ***//
+function subtract(n1, n2) {
+
+}
+//*** ES6 ***//
+const subtract = (n1,n2) =>  // write your code here
+
+//--------------------------------------------------------------------------------//
+// multiplies the inputted number by 2
+// ex: multiplyByTwo(5); -> 10
+// ex: multiplyByTwo(6); -> 12
+//*** ES5 ***//
+function multiplyByTwo(numero) {
+
+}
+//*** ES6 ***//
+const multiplyByTwo = numero => // write your code here
+
+//--------------------------------------------------------------------------------//
+//*** ES5 ***//
+// multplies the two inputted numbers together
+// ex: multiply(1,2); -> 2
+// ex: multiply(2,2); -> 4
+function multiply(val1, val2) {
+
+}
+//*** ES6 ***//
+// write your code here
+
+//--------------------------------------------------------------------------------//
+//*** ES5 ***//
+// multiplies all three inputted numbers together
+// ex: multiplyy(1,2,3); -> 6
+// ex: multiplyy(2,2,3); -> 12
+function multiplyy(a, b, c) {
+
+}
+//*** ES6 ***//
+// write your code here
+
+//--------------------------------------------------------------------------------//
+//*** ES5 ***//
+// returns the square of the inputted number
+// ex: square(5); -> 25
+function square(value) {
+
+}
+//*** ES6 ***//
+// write your code here
+
+//--------------------------------------------------------------------------------//
+//*** ES5 ***//
+// determines if the number is odd
+// outputs true if the number is odd
+// outputs false if the number is even
+// ex: isOdd(4); -> true
+// ex: isOdd(5); -> false
+function isOdd(num) {
+
+}
+//*** ES6 ***//
+// write your code here
+
+//--------------------------------------------------------------------------------//
+//*** ES5 ***//
+// determines if the number is negative
+// outputs true if the number is negative
+// outputs false if the number is zero or positive
+// ex: isNegative(-1); -> true
+// ex: isNegative(1); -> false
+// ex: isNegative(0); -> false
+function isNegative(num) {
+
+}
+//*** ES6 ***//
+// write your code here
+
+//--------------------------------------------------------------------------------//
+//*** ES5 ***//
+// returns the absolute value of the inputted number
+// ex: positive(-1); -> 1
+// ex: positive(1); -> 1
+function positive(num) {
+
+}
+//*** ES6 ***//
+// write your code here
+
+//--------------------------------------------------------------------------------//
+//*** ES5 ***//
+// Returns boolean of whether argument is classified as a Number object
+// isNumber(5); → true
+// isNumber('hi'); → false
+function isNumber(value) {
+
+}
+//*** ES6 ***//
+// write your code here
+
+//--------------------------------------------------------------------------------//
+//*** ES5 ***//
+// Returns boolean of whether argument is classified as an Array object
+// isArray(5); → false
+// isArray([1,2,3]); → true
+function isArray(value) {
+
+}
+//*** ES6 ***//
+// write your code here
+
+//--------------------------------------------------------------------------------//
+//*** ES5 ***//
+// Returns boolean of whether argument is classified as an Object
+// isObject(5); → false
+// isObject([1,2,3]); → false
+function isObject(value) {
+
+}
+//*** ES6 ***//
+// write your code here
+
+//--------------------------------------------------------------------------------//
+//*** ES5 ***//
+// return boolean of whether argument is classified as null
+// isNull(null); -> true
+// isNull(5); -> false
+function isNull(value) {
+
+}
+//*** ES6 ***//
+// write your code here
+
+//--------------------------------------------------------------------------------//
