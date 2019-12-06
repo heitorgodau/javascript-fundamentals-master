307259864891a865766417175f71a4f489dcdbe2
Monica Damasceno <monicadamasceno@gmail.com>
2019-12-06 10:54:36 -0200

    add loop exercises

--- loop.js
+++ loop.js
@@ -1,3 +1,5 @@
+//! READ: http://learn.ironhack.com/#/learning_unit/8206
+
 // print numbers 0 to 100
 /*code here */
 
@@ -13,7 +15,35 @@
 // print numbers 80 to 20
 /*code here */
 
+
 const pets = ['dog', 'cat', 'bird', 'salamander', 'human', 'ape', 'alien', 'dinossaur'];
 // Use FOR Loop to print the content of array "pets"
 
+
+
+// Use FOR EACH to print the content of array "pets"
+
+
+
+const array = [1,'x',3,8,'x',5,7,3,'x',2,6,'x',10]
+//Use FOR to count and print how many times the letter x appears in the array
+
+
+//Use FOR EACH to count and print how many times the letter x appears in the array
+
+
+
+const numbers = [1,5,6,8,12,25,65,78]
+//Use FOR to calculate and print the sum of all numbers in the array
+
+
+//Use FOR EACH to calculate and print the sum of all numbers in the array
+
+
+
+const sentence = 'Hack your life'
+//Use FOR to print each letter of the sentence on a different line.
+
+
+//Use FOR EACH to print each letter of the sentence on a different line.
 
