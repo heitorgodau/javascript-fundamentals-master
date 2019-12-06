2e9ac0970a5a954bad15c6715cb9792205592f94
Heitor Godau <heitorgodau@gmail.com>
2019-12-06 11:11:36 -0300

    loops exercise added

--- loop.js
+++ loop.js
@@ -1,19 +1,17 @@
 // print numbers 0 to 100
-/*code here */
+/* code here */
 
 
 // print only even numbers from 0 to 100
-/*code here */
+/* code here */
 
 
 // print only odd numbers from 0 to 100
-/*code here */
+/* code here */
 
 
 // print numbers 80 to 20
-/*code here */
+/* code here */
 
 const pets = ['dog', 'cat', 'bird', 'salamander', 'human', 'ape', 'alien', 'dinossaur'];
 // Use FOR Loop to print the content of array "pets"
-
-
